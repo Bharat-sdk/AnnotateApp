f3ae3f5cbbbe711ada7646a6b44035eef5379774
Bharat-sdk <bk9735735777@gmail.com>
2021-06-11 09:57:20 +0530

    annotation app

--- app/src/main/java/com/sippitechnologes/annotateapp/MainActivity.java
+++ app/src/main/java/com/sippitechnologes/annotateapp/MainActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 Button getAnnotate,getCancel;
 ImageButton annotate,show_cropping_area,red_box,blue_box,green_box,yellow_box,red_txt,blue_txt,green_txt,yellow_txt;
+ImageButton save,refresh;
+ImageView imageView;
 TextView label;
 AlertDialog alertDialog;
 IconCropView iconCropView;
+Uri gallery_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,11 @@ IconCropView iconCropView;
         blue_txt= findViewById(R.id.blue_color_txt);
         green_txt = findViewById(R.id.green_color_txt);
         yellow_txt = findViewById(R.id.yellow_color_txt);
+        //Image refresh and save
+        save = findViewById(R.id.save);
+        refresh = findViewById(R.id.refresh);
+        //imageview
+        imageView = findViewById(R.id.crop_image);
 
         annotate.setOnClickListener(this);
         show_cropping_area.setOnClickListener(this);
@@ -52,6 +65,18 @@ IconCropView iconCropView;
         green_txt.setOnClickListener(this::onClick);
         yellow_txt.setOnClickListener(this::onClick);
         blue_txt.setOnClickListener(this::onClick);
+        save.setOnClickListener(this::onClick);
+        refresh.setOnClickListener(this::onClick);
+
+        //Intent getting the image and setting it from camera or gallery
+        Intent i = getIntent();
+       String name= i.getStringExtra("label");
+        Log.d("TAG", "onCreate: "+name);
+
+       if (name.equals("labelfromgallery")) {
+           gallery_image = i.getParcelableExtra("path");
+           imageView.setImageURI(gallery_image);
+       }
 
 
 
@@ -148,6 +173,16 @@ IconCropView iconCropView;
                 iconCropView.invalidate();
             }
             break;
+            case R.id.save:
+            {
+                ConstraintLayout view = (ConstraintLayout) findViewById(R.id.pic_taker);
+                view.setDrawingCacheEnabled(true);
+                view.buildDrawingCache();
+                Bitmap bm = view.getDrawingCache();
+                iconCropView.setVisibility(View.INVISIBLE);
+                imageView.setImageBitmap(bm);
+
+            }
 
 
         }
